Fixed serious bugs with the calculation and plot of instance importance scores
I had forgotten to deactivate dropout, so it was giving stochastic outputs, the sorting method was messing up the scores and the colorscale wasn't working properly. Now the priority is to find a way to make the colors stand out more, perhaps by applying a non-linear function to the scores. Besides that, I could also account for the variation brought into the output, comparing to the previous one, through a weighted average, as well as adapt the plot's zoom so that it always has the same scale.

FCUL_ALS_model_interpretability.py
@@ -286,7 +286,10 @@ shap.summary_plot(shap_values.reshape(-1, model.lstm.input_size), features=test_
 interpreter = ModelInterpreter(model, ALS_df, seq_len_dict, fast_calc=False, SHAP_bkgnd_samples=200)
 
 # + {"pixiedust": {"displayParams": {}}}
-interpreter.interpret_model(bkgnd_data=train_features, test_data=test_features[:10, :, :], test_labels=test_labels[:10, :], instance_importance=True, feature_importance=False)
+# Number of patients to analyse
+n_patients = 20
+
+interpreter.interpret_model(bkgnd_data=train_features, test_data=test_features[:n_patients], test_labels=test_labels[:n_patients], instance_importance=True, feature_importance=False)
 
 # +
 # Get the current day and time to attach to the saved model's name
@@ -308,18 +311,19 @@ with open(interpreter_filename, 'wb') as file:
 with open('GitHub/FCUL_ALS_Disease_Progression/interpreters/checkpoint_14_05_2019_02_12.pickle', 'rb') as file:
     interpreter_loaded = pickle.load(file)
 
-np.array_equal(interpreter_loaded.feat_scores, interpreter.feat_scores)
+if np.array_equal(interpreter_loaded.feat_scores, interpreter.feat_scores):
+    interpreter = interpreter_loaded
 
 # ### Feature importance plots
 
 # Summarize the effects of all the features
-shap.summary_plot(interpreter_loaded.feat_scores.reshape(-1, interpreter_loaded.model.lstm.input_size), 
-                  features=test_features_denorm[:, :, 2:].view(-1, interpreter_loaded.model.lstm.input_size).numpy(), 
+shap.summary_plot(interpreter.feat_scores.reshape(-1, interpreter.model.lstm.input_size), 
+                  features=test_features_denorm[:, :, 2:].view(-1, interpreter.model.lstm.input_size).numpy(), 
                   feature_names=ALS_cols, plot_type='bar')
 
 # Summarize the effects of all the features
-shap.summary_plot(interpreter_loaded.feat_scores.reshape(-1, model.lstm.input_size), 
-                  features=test_features_denorm[:, :, 2:].contiguous().view(-1, interpreter_loaded.model.lstm.input_size).numpy(), 
+shap.summary_plot(interpreter.feat_scores.reshape(-1, model.lstm.input_size), 
+                  features=test_features_denorm[:, :, 2:].contiguous().view(-1, interpreter.model.lstm.input_size).numpy(), 
                   feature_names=ALS_cols, plot_type='violin')
 
 # +
@@ -338,7 +342,7 @@ seq_len = seq_len_dict[test_features_denorm[patient, 0, 0].item()]
 #                 features=test_features_denorm[patient, :seq_len, 2:].numpy(), 
 #                 feature_names=ALS_cols)
 shap.force_plot(0.2, 
-                interpreter_loaded.feat_scores[patient, :seq_len], 
+                interpreter.feat_scores[patient, :seq_len], 
                 features=test_features_denorm[patient, :seq_len, 2:].numpy(), 
                 feature_names=ALS_cols)
 # + {}
@@ -355,19 +359,17 @@ ts = 1
 #                 features=test_features_denorm[patient, ts, 2:].numpy(), 
 #                 feature_names=ALS_cols)
 shap.force_plot(0.2, 
-                interpreter_loaded.feat_scores[patient][ts], 
+                interpreter.feat_scores[patient][ts], 
                 features=test_features_denorm[patient, ts, 2:].numpy(), 
                 feature_names=ALS_cols)
 # -
 # ### Instance importance plots
 
-interpreter_loaded = interpreter
-
 # interpreter_loaded.inst_scores[interpreter_loaded.inst_scores == interpreter_loaded.padding_value]
-interpreter_loaded.inst_scores[interpreter_loaded.inst_scores == 999999] = np.nan
-interpreter_loaded.inst_scores
+interpreter.inst_scores[interpreter.inst_scores == 999999] = np.nan
+interpreter.inst_scores
 
-inst_scores_avg = np.nanmean(interpreter_loaded.inst_scores, axis=0)
+inst_scores_avg = np.nanmean(interpreter.inst_scores, axis=0)
 
 list(range(1, len(inst_scores_avg)+1))
 
@@ -391,8 +393,6 @@ patient = 0
 seq_len = seq_len_dict[test_features_denorm[patient, 0, 0].item()]
 
 # Plot the instance importance of one sequence
-configure_plotly_browser_state()
-
 data = [go.Scatter(
                     x = list(range(seq_len)),
                     y = interpreter_loaded.inst_scores[patient, :seq_len],
@@ -408,7 +408,7 @@ py.iplot(fig)
 
 # +
 # Choosing which example to use
-patient = 0
+patient = 8
 
 # True sequence length of the current patient's data
 seq_len = seq_len_dict[test_features_denorm[patient, 0, 0].item()]
@@ -417,19 +417,22 @@ seq_len = seq_len_dict[test_features_denorm[patient, 0, 0].item()]
 interpreter.instance_importance_plot(test_features_denorm, interpreter.inst_scores, patient, seq_len)
 # -
 
-# **Comments:**
-# * The downward spike at instances 5, 6 and 7 coincide with the instances where higher positive SHAP values appear (e.g. 3r = 2, p10 = 1), as seen on the SHAP plot of the same patient's sequence.
-# * It doesn't make sense that all instances in a sequence have so big absolute importance scores. That would mean that all instances are somehow super important, yet at the same time just removing any single one of them would change the output completely.
-
-ref_output = interpreter_loaded.model(test_features[0, :, 2:].float().unsqueeze(0), [x_lengths_test[0]])
+ref_output = interpreter.model(test_features[patient, :, 2:].float().unsqueeze(0), [x_lengths_test[patient]])
 ref_output
+
+len(ref_output)
+
+x_lengths_test[patient]
 
 ref_output[-1].item()
 
+ref_output2, _ = utils.model_inference(interpreter.model, interpreter.seq_len_dict, data=(test_features[patient].unsqueeze(0), test_labels[patient].unsqueeze(0)),
+                                       metrics=[''], seq_final_outputs=True)
+ref_output2.item()
+
 # +
-patient = 0
 instance = 0
-sequence_data = test_features[0, :, 2:].float()
+sequence_data = test_features[patient, :, 2:].float()
 x_length = x_lengths_test[patient]
 
 # Indeces without the instance that is being analyzed
@@ -443,55 +446,36 @@ sequence_data = sequence_data[instances_idx, :]
 sequence_data = sequence_data.unsqueeze(0)
 
 # Calculate the output without the instance that is being analyzed
-new_output = interpreter_loaded.model(sequence_data, [x_length-1])
+new_output = interpreter.model(sequence_data, [x_length-1])
 new_output[-1].item()
 # -
+
+test_features[patient, :, 2:].shape
+
+sequence_data.shape
 
 ref_output[-1].item() - new_output[-1].item()
 
 # **Comments:**
 # * The fact that the result of the last cell (-6.099371239542961e-05) is so different from the instance importance score assigned to the instance (-0.974) proves that something is clearly wrong in the instance_importance method.
 
-test_features[:5, :, 2:]
-
-test_features[:5, :, 2:].shape
-
-x_lengths_test[:5]
-
-# + {"pixiedust": {"displayParams": {}}}
-output = interpreter_loaded.model(test_features[:5, :, 2:].float(), x_lengths_test[:5])
-output[:20]
-# -
-
-output[19]
-
-sum(x_lengths_test[:5])
-
-5*20
-
 x_lengths_test_cumsum = np.cumsum(x_lengths_test[:5])
 x_lengths_test_cumsum
 
-x_lengths_test_cumsum_shifted = np.roll(x_lengths_test_cumsum, 1)
-x_lengths_test_cumsum_shifted[0] = 0
-x_lengths_test_cumsum_shifted
-
 output[x_lengths_test_cumsum-1]
 
-pred_prob, _ = utils.model_inference(interpreter.model, interpreter.seq_len_dict, data=(test_features[:10], test_labels[:10]),
+pred_prob, _ = utils.model_inference(interpreter.model, interpreter.seq_len_dict, data=(test_features[:n_patients], test_labels[:n_patients]),
                                      metrics=[''], seq_final_outputs=True)
 pred_prob
 
 pred_prob.shape
 
-test_features[:10].shape
+test_features[:n_patients].shape
 
 interpreter.inst_scores.shape
 
-interpreter.instance_importance_plot(test_features[:10], interpreter.inst_scores, pred_prob=pred_prob)
+test_features[:n_patients, 0, 0]
 
-len(interpreter.inst_scores.shape)
-
-type(None) is list
+interpreter.instance_importance_plot(test_features[:n_patients], interpreter.inst_scores, pred_prob=pred_prob)
 
 

ModelInterpreter.py
@@ -83,6 +83,9 @@ class ModelInterpreter:
         self.feat_names = feat_names
         self.padding_value = padding_value
 
+        # Put the model in evaluation mode to deactivate dropout
+        self.model.eval()
+
         if type(data) is torch.Tensor:
             self.data = data
             self.labels = labels
@@ -179,6 +182,8 @@ class ModelInterpreter:
         seq_len_dict = dict([(idx, val[0]) for idx, val in list(zip(seq_len_df.index, seq_len_df.values))])
         return seq_len_dict
 
+    # [TODO] Analyse other options for the calculation of instance importance scores
+    # (e.g. scaling with logarithm or sigmoid, average with variation of output since the previous instance, etc)
     def instance_importance(self, data=None, labels=None, x_lengths=None, see_progress=True):
         '''Calculate the instance importance scores to interpret the impact of
         each instance of a sequence on the final output.
@@ -258,8 +263,10 @@ class ModelInterpreter:
             # DEBUG
             # inst_scores = []
             # for seq_num in self.progress_bar(range(data.shape[0])):
+            #     tmp_list = []
             #     for inst in range(x_lengths[seq_num]):
-            #         inst_scores.append(calc_instance_score(data[seq_num, :, :], inst, ref_output[seq_num], x_lengths[seq_num]))
+            #         tmp_list.append(calc_instance_score(data[seq_num, :, :], inst, ref_output[seq_num], x_lengths[seq_num]))
+            #     inst_scores.append(tmp_list)
         else:
             inst_scores = [[calc_instance_score(data[seq_num, :, :], inst, ref_output[seq_num], x_lengths[seq_num])
                             for inst in range(x_lengths[seq_num])] for seq_num in range(data.shape[0])]
@@ -576,7 +583,7 @@ class ModelInterpreter:
                                                                   neg_color=NEG_COLOR))
                           )]
             layout = go.Layout(
-                                title=f'Instance importance scores for ID {int(orig_data[0, 0, self.id_column])}',
+                                title=f'Instance importance scores for ID {int(orig_data[id, 0, self.id_column])}',
                                 xaxis=dict(title='Instance'),
                                 yaxis=dict(title='Importance scores')
                               )
@@ -591,8 +598,8 @@ class ModelInterpreter:
             pred_prob = pred_prob[id]
 
             # Unique patient ids in string format
-            patients = [str(int(item)) for item in set([tensor.item()
-                        for tensor in list(orig_data[:, 0, self.id_column])])]
+            patients = [str(int(item)) for item in [tensor.item()
+                        for tensor in list(orig_data[:, 0, self.id_column])]]
 
             # Sequence instances count, used as X in the plot
             seq_insts_x = [list(range(inst_scores.shape[1]))
@@ -606,7 +613,7 @@ class ModelInterpreter:
             patients_y = list(np.array(patients_y).flatten())
 
             # Define colors for the data points based on their normalized scores (from 0 to 1 instead of -1 to 1)
-            colors = [(val+1)/2 for val in inst_scores.flatten()]
+            colors = [val for val in inst_scores.flatten()]
 
             # Count the number of already deleted paddings
             count = 0
@@ -682,6 +689,7 @@ class ModelInterpreter:
             # Prediction probabilities in text form, to appear in the plot
             text_content = [pred_prob[idx] for idx in range(len(pred_prob)) for i in range(10)]
 
+            # [TODO] Ajdust the zoom so that the plot always has the same scale
             plot_data = [{"x": seq_insts_x,
                           "y": patients_y,
                           "marker": dict(color=colors, size=12,
@@ -689,8 +697,10 @@ class ModelInterpreter:
                                                       color = 'black',
                                                       width = 1
                                                     ),
-                                         colorbar=dict(title='Normalized scores'),
-                                         colorscale=[[0, 'rgba(30,136,229,1)'], [0.5, 'white'], [1, 'rgba(255,13,87,1)']]),
+                                         colorbar=dict(title='Scores'),
+                                         colorscale=[[0, 'rgba(30,136,229,1)'], [0.5, 'white'], [1, 'rgba(255,13,87,1)']],
+                                         cmax=1,
+                                         cmin=-1),
                           "mode": "markers",
                           "type": "scatter",
                           "hoverinfo": 'x+y'
@@ -701,7 +711,7 @@ class ModelInterpreter:
                                      text=text_content,
                                      mode='text',
                                      textfont=dict(size = 1, color='#ffffff'),
-                                     hoverinfo='text'
+                                     hoverinfo='y+text'
                          )]
             layout = go.Layout(
                                 title="Patients time series",

utils.py
@@ -685,20 +685,29 @@ def sort_by_seq_len(data, seq_len_dict, labels=None, id_column=0):
     # Get the original lengths of the sequences, for the input data
     x_lengths = [seq_len_dict[id] for id in list(data[:, 0, id_column].numpy())]
 
-    # Sorted indeces to get the data sorted by sequence length
-    data_sorted_idx = list(np.argsort(x_lengths)[::-1])
+    is_sorted = all(x_lengths[i] >= x_lengths[i+1] for i in range(len(x_lengths)-1))
 
-    # Sort the x_lengths array by descending sequence length
-    x_lengths = [x_lengths[idx] for idx in data_sorted_idx]
+    if is_sorted:
+        # Do nothing if it's already sorted
+        sorted_data = data
+        sorted_labels = labels
+    else:
+        # Sorted indeces to get the data sorted by sequence length
+        data_sorted_idx = list(np.argsort(x_lengths)[::-1])
 
-    # Sort the data by descending sequence length
-    sorted_data = data[data_sorted_idx, :, :]
+        # Sort the x_lengths array by descending sequence length
+        x_lengths = [x_lengths[idx] for idx in data_sorted_idx]
+
+        # Sort the data by descending sequence length
+        sorted_data = data[data_sorted_idx, :, :]
+
+        if labels is not None:
+            # Sort the labels by descending sequence length
+            sorted_labels = labels[data_sorted_idx, :]
 
     if labels is None:
         return sorted_data, x_lengths
     else:
-        # Sort the labels by descending sequence length
-        sorted_labels = labels[data_sorted_idx, :]
         return sorted_data, sorted_labels,  x_lengths
 
 
@@ -830,6 +839,9 @@ def model_inference(model, seq_len_dict, dataloader=None, data=None, metrics=['l
         Dictionary containing the calculated performance on each of the
         specified metrics.
     '''
+    # Guarantee that the model is in evaluation mode, so as to deactivate dropout
+    model.eval()
+
     # Create an empty dictionary with all the possible metrics
     metrics_vals = {'loss': None,
                     'accuracy': None,